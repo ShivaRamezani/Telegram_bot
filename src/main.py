de1ad37219aea142645ad3cb72441144cb852acb
Shiva Ramezani <shivaramezani@DESKTOP-GUFGRR0.localdomain>
2022-08-20 13:37:59 -0700

    emojizing the markups and bolding the name of the user

--- src/main.py
+++ src/main.py
@@ -1,31 +1,45 @@
 import telebot
 import os
+from telebot import types
 from loguru import logger
+from src.utils import create_keyboard
+from src.constants import KEYBOARDS
+import emoji
 
 #print(os.environ["TELEGRAMBOT_TOKEN"])
 class Bot:
     def __init__(self):
-        self.bot = telebot.TeleBot(os.environ["TELEGRAMBOT_TOKEN"], parse_mode=None)
+        self.bot = telebot.TeleBot(os.environ["TELEGRAMBOT_TOKEN"], parse_mode='HTML')
         self.respond_welcome=self.bot.message_handler(commands=['start', 'help'])(self.respond_welcome)
         self.respond_text=self.bot.message_handler(func=lambda message: True)(self.respond_text)
 
 
     def respond_welcome(self, message):
         self.set(message)
-	    self.bot.reply_to(message, f"Hey {self.name}!")
+        self.send_message(f"Hey <b>{self.name}</b>!",reply_markup=KEYBOARDS.main)
 
 
     def respond_text(self, message):
+        logger.info(emoji.demojize(message.text))
         self.set(message)
-	    self.bot.send_message(message.chat.id, f'Hey {self.name}!')
+        self.send_message(f'Hey {self.name}!')
 
 
     def set(self, message):
+        self.message=message
         self.name = message.chat.first_name
         if message.chat.last_name:
-            self.name = f"{self.name} {self.last_name}"
+            self.name = f"{self.name} {message.chat.last_name}"
         self.chat_id = message.chat.id
-        logger.info(f'chat id: {self.chat_id}')
+
+
+    def send_message(self, bot_response, chat_id=None, reply_markup=None, emojize=True):
+        if emojize:
+            bot_response = emoji.emojize(bot_response)
+        if not chat_id:
+            chat_id = self.message.chat.id
+        
+        self.bot.send_message(chat_id, bot_response, reply_markup=reply_markup)
 
 
     def run(self):
