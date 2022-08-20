e94516fe8b09a26cf4565fb0cb11c50443e58273
Shiva Ramezani <shivaramezani@DESKTOP-GUFGRR0.localdomain>
2022-08-20 00:20:54 -0700

    changing response for all type message types

--- src/main.py
+++ src/main.py
@@ -6,10 +6,26 @@ from loguru import logger
 class Bot:
     def __init__(self):
         self.bot = telebot.TeleBot(os.environ["TELEGRAMBOT_TOKEN"], parse_mode=None)
-        self.send_welcome(message)=self.bot.message_handler(commands=['start', 'help'])
+        self.respond_welcome=self.bot.message_handler(commands=['start', 'help'])(self.respond_welcome)
+        self.respond_text=self.bot.message_handler(func=lambda message: True)(self.respond_text)
 
-    def send_welcome(message):
-	    bot.reply_to(message, "Hey!")
+
+    def respond_welcome(self, message):
+        self.set(message)
+	    self.bot.reply_to(message, f"Hey {self.name}!")
+
+
+    def respond_text(self, message):
+        self.set(message)
+	    self.bot.send_message(message.chat.id, f'Hey {self.name}!')
+
+
+    def set(self, message):
+        self.name = message.chat.first_name
+        if message.chat.last_name:
+            self.name = f"{self.name} {self.last_name}"
+        self.chat_id = message.chat.id
+        logger.info(f'chat id: {self.chat_id}')
 
 
     def run(self):
@@ -17,8 +33,11 @@ class Bot:
         self.bot.polling()
 
 
+
 if __name__ == '__main__':
     bot = Bot()
+    bot.run()
+
 
 
 
